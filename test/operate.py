0213e6b84c169206e72db1a4c5808e0288e96a47
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-09-06 22:53:29 -0600

    add a test yaml file and test.sh script for a simple regression test

---
+++ test/operate.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import os
+import sys
+
+# for hifiyaml tests, we use the hifiyaml in this repo
+#   instead of the installed hifiyaml
+here = os.path.dirname(__file__)
+hifiyaml_path = os.path.join(here, "..")
+if hifiyaml_path not in sys.path:
+    sys.path.insert(0, hifiyaml_path)
+import hifiyaml as hy
+
+args = sys.argv
+nargs = len(args) - 1
+if nargs < 3:
+    print(f"{os.path.basename(args[0])} <file> <dump|drop|modify> <querystr> [newblock] [nodedent]")
+    exit()
+
+newblock, dedent = "", True   # default values
+
+yfile = args[1]
+operator = args[2]
+if nargs > 2:
+    querystr = args[3]
+else:
+    hy.printd("!!no querystr provided!!")
+    sys.exit(1)
+if nargs > 3:
+    if operator == "modify":
+        newblock = args[4]
+    elif operator == "dump":
+        dedent = (args[4] != "nodedent")
+
+data =  hy.load(yfile)
+
+if operator == "dump":
+    hy.dump(data, querystr, do_dedent=dedent)
+elif operator == "drop":
+    hy.drop(data, querystr)
+elif operator == "modify":
+    if newblock:
+        hy.modify(data, querystr, newblock)
+    else:
+        print("newblock cannot be empty for the modify operator")
---
+++ test/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm -rf tmp
+mkdir -p tmp
+operate.py demo.yaml dump "cost function/observations/observers/0/obs filters/0" > tmp/0.yaml
+operate.py demo.yaml dump "cost function/observations/observers/0/obs filters/1" > tmp/1.yaml
+operate.py demo.yaml dump "cost function/observations/observers/0/obs filters/2" > tmp/2_0.yaml
+operate.py demo.yaml dump "cost function/observations/observers/0/obs filters/2" nodedent > tmp/2_1.yaml
+operate.py demo.yaml dump "cost function/observations/observers/0/obs filters/3" > tmp/3.yaml
+operate.py demo.yaml dump "cost function/observations/observers/0/obs filters/4" > tmp/4.yaml
+operate.py demo.yaml dump "cost function/observations/observers/0/obs filters/5" > tmp/5.yaml
+operate.py demo.yaml dump "cost function/observations/observers/0/obs filters/6" > tmp/6.yaml
+
+diff tmp ref
+if (( $? == 0 )); then
+  echo "test passed, identifical results."
+else
+  echo "test failed, different reulsts!"
+fi
